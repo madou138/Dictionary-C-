99ff5eb1346020539eb1b5c8c8083e3cd487dc14
Mamadou Thiello <madou138@gmail.com>
2018-11-16 00:14:00 -0500

    first commit

---
+++ Dictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(GetWebstersWords());
+            Console.ReadLine();//keep the console on the screen
+        }
+
+        public static List<Meta> GetWebstersWords()
+        {
+            var result = new List<Meta>();//will hold our deserialized data
+            var webClient = new WebClient();
+            byte[] webstersWords = webClient.DownloadData("https://dictionaryapi.com/api/v3/references/collegiate/json/big?key=5c3ec0b3-41ae-4bfb-a793-d467709d3d98");
+            var serializer = new JsonSerializer();
+            using (var stream = new MemoryStream(webstersWords))
+            using (var reader = new StreamReader(stream))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+               result = serializer.Deserialize<SearchWords>(jsonReader).Metas;
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ Dictionary/Search.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+
+    public class SearchWords
+    {
+        public Wc_Link wc_link { get; set; }
+        [JsonProperty(PropertyName = "meta")]
+        public List<Meta> Metas { get; set; }
+        public Hwi hwi { get; set; }
+        public string fl { get; set; }
+        public Def1[] def { get; set; }
+        public Uro[] uros { get; set; }
+        public string[][] et { get; set; }
+        [JsonProperty(PropertyName = "date")]
+        public string Date { get; set; }
+        [JsonProperty(PropertyName="ld_link")]
+        public Ld_Link LdLlink { get; set; }
+        public Suppl suppl { get; set; }
+        public string[] shortdef { get; set; }
+    }
+
+    public class Wc_Link
+    {
+        public string link_hw { get; set; }
+    }
+
+    public class Meta
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        public string uuid { get; set; }
+        public string sort { get; set; }
+        public string update { get; set; }
+        public string editdate { get; set; }
+        [JsonProperty(PropertyName = "release")]
+        public string Release { get; set; }
+        public string src { get; set; }
+        public string section { get; set; }
+        public string[] stems { get; set; }
+        public bool offensive { get; set; }
+    }
+
+    public class Hwi
+    {
+        public string hw { get; set; }
+        public Pr[] prs { get; set; }
+    }
+
+    public class Pr
+    {
+        public string mw { get; set; }
+        public Sound sound { get; set; }
+    }
+
+    public class Sound
+    {
+        public string audio { get; set; }
+        public string _ref { get; set; }
+        public string stat { get; set; }
+    }
+
+    public class Ld_Link
+    {
+        public string link_hw { get; set; }
+        public string link_fl { get; set; }
+    }
+
+    public class Suppl
+    {
+        public Example[] examples { get; set; }
+        public Ldq ldq { get; set; }
+    }
+
+    public class Ldq
+    {
+        public string ldhw { get; set; }
+        public string fl { get; set; }
+        public Def[] def { get; set; }
+    }
+
+    public class Def
+    {
+        public string[] sls { get; set; }
+        public object[][][] sseq { get; set; }
+    }
+
+    public class Example
+    {
+        public string t { get; set; }
+    }
+
+    public class Def1
+    {
+        public object[][][] sseq { get; set; }
+    }
+
+    public class Uro
+    {
+        public string ure { get; set; }
+        public string fl { get; set; }
+    }
+
+}
