5ed79814420f4d4be43af6eebbfe289d20ba3338
Mamadou Thiello <madou138@gmail.com>
2018-12-22 17:43:55 -0500

    shortened the code

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -13,26 +13,20 @@ namespace Dictionary
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine(GetWebstersWords());
+            GetWebstersWords();
             Console.ReadLine();//keep the console on the screen
         }
 
-        public static Meta GetWebstersWords()
+        public static void GetWebstersWords()
         {
-            SearchWords searchwords = new SearchWords();
-            var result = new Meta();//will hold our deserialized data
             var webClient = new WebClient();
             byte[] webstersWords = webClient.DownloadData("https://dictionaryapi.com/api/v3/references/collegiate/json/big?key=yourkey");
-            var serializer = new JsonSerializer();
             using (var stream = new MemoryStream(webstersWords))
             using (var reader = new StreamReader(stream))
-            using (var jsonReader = new JsonTextReader(reader))
             {
-               result = (Meta) (serializer.Deserialize(jsonReader));
+                reader.ReadToEnd();
             }
 
-            return result;
         }
     }
 }
